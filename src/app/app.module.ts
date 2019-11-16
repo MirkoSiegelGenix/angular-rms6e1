7eeec06d8012fc6479087ef3b1826d31fa15484b
MirkoSiegelGenix <msiegel@genixventures.com>
2019-11-16 15:58:56 +1100

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
@@ -19,7 +20,8 @@ import { CartComponent } from './cart/cart.component';
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent},
       { path: 'cart', component: CartComponent },
-    ])
+    ]),
+    HttpClientModule
   ],
   declarations: [
     AppComponent,
